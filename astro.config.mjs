57eb931aebdb7dcafaf3152683c79671e9f765c6
Calvin Kamp <calvin.kamp@active-value.de>
2024-02-09 11:02:05 +0100

    integrated storyblok as cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,27 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+
+export default defineConfig({
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true,
+        },
+    },
+    integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                page: 'storyblok/Page',
+                feature: 'storyblok/Feature',
+                grid: 'storyblok/Grid',
+                teaser: 'storyblok/Teaser',
+            },
+        }),
+    ],
+})
